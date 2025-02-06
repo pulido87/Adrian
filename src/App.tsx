f750e197b146ffe50bbd56b18e70117fd529fc21
pulido87 <58183797+pulido87@users.noreply.github.com>
2025-02-06 11:51:49 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
+import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Gallery />
       <Contact />
     </div>
   );
